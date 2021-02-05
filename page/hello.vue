27624bad61f9a4c8d5546aba761e7ab90e754792
shehuan <shehuan@nowledgedata.com.cn>
2021-02-05 13:42:22 +0800

    单文件组件

---
+++ page/hello.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>{{greeting}}</p>
+</template>
+
+<script>
+    module.exports = {
+        data: function () {
+            return {
+                greeting: 'hello'
+            }
+        }
+    }
+</script>
+
+<style>
+    p {
+        font-size: 2em;
+        text-align: center;
+    }
+</style>
